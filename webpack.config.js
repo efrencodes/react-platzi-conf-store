5e5d9a45d89670dcebde161cf254fda4ef486170
efrenmartinez <developer.emartinez@gmail.com>
2021-08-02 14:37:02 -0500

    :wrench: Add configuration eslint prettier

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // https://www.npmjs.com/package/mini-css-extract-plugin
 
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3005
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    'css-loader'
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].css',
-        }),
-    ],
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3005
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: {
+					loader: 'html-loader'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].css'
+		})
+	]
 }
